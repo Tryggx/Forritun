aba48a113a24e6f6ccc113e1bd9fa01a0585a42d
Tryggx <tryggx97@gmail.com>
2020-09-03 15:36:20 +0000

    Forritun

--- sequence.py
+++ sequence.py
@@ -1,2 +1,15 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-dfsdfsdfsdfsdf
+n1=0
+n2=1
+n3=2
+
+print(n2)
+
+if (n>1):
+    print(n3)
+for i in range (1,n-1):
+    n4=n1+n2+n3
+    n1=n2
+    n2=n3
+    n3=n4
+    print (n4)
